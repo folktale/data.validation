ea82ead026d55f4409c93a0de00f923c85c29f53
Quildreen "Sorella" Motta <quildreen@gmail.com>
2013-12-15 14:13:20 -0200

    Fixes typo on documentation.
    --HG--
    branch : pages

--- assets/behavior.js
+++ assets/behavior.js
@@ -21,8 +21,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 1,
-            "title": "Applivative: Validation",
-            "slug": "applivative-validation",
+            "title": "Applicative: Validation",
+            "slug": "applicative-validation",
             "isFileHeader": true
           },
           "depth": 1,
@@ -105,7 +105,7 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             }
           ]
         },
-        "title": "Applivative: Validation"
+        "title": "Applicative: Validation"
       },
       "depth": 1,
       "outline": [
